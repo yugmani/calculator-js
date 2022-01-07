6b331cd7e89f90004f873221e0ea8cb2facc9c4e
Himalaya <yugmani@yahoo.com>
2022-01-06 23:59:05 -0800

    javascript functions

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # calculator-js
+
+## Simple Calculator using JavaScript
+
+- A user can input a value
+- A user can select arithmetical operator (symbol)
+- A user can input a second value
+- A user can see the result of arithmetical operation after clicking on equal(=) sign.
+- A user can click on Clear Button (C) to clear the screen value.
+- If a user tries to see the result without input value and arithmetical operator, alert will appear in the screen.
+
+## Technology Used
+
+- HTML
+- CSS
+- JavaScript
--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hellow World!");
+const btnAll = document.querySelectorAll(".btn");
+const screen = document.querySelector(".screen");
+const btnEqual = document.querySelector(".btn-equal");
+const btnClear = document.querySelector(".btn-clear");
+
+for (let i = 0; i < btnAll.length; i++) {
+  btnAll[i].addEventListener("click", function () {
+    let inputValue = btnAll[i].getAttribute("data-num");
+    screen.value += inputValue;
+  });
+}
+
+btnEqual.addEventListener("click", function () {
+  if (screen.value === "") {
+    screen.value = "Input Required";
+    screen.style.color = "red";
+
+    setTimeout(function () {
+      window.location.reload();
+    }, 1000);
+  }
+
+  let result = eval(screen.value);
+  screen.value = result;
+});
+
+btnClear.addEventListener("click", function () {
+  screen.value = "";
+});
